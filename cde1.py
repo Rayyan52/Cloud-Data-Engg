9c9e2b5ba5b37554bc98bdf1565ac44b80a2ebfa
Rayyan52 <syed.rayyan52@gmail.com>
2024-12-15 15:30:22 +0500

    file handling

--- cde1.py
+++ cde1.py
@@ -1,14 +1,16 @@
-# print("Hey cloud")
-# print("Saylani class 1")
-# print("Python Programming")
-# print("PYTHON 4 EVERYONE")
-# print("Python is easy to learn")
-# print("Python is easy to learn")
-# print(1+1)
-# print("Hello rayyan and nabeedah")
+# # print("Hey cloud")
+# # print("Saylani class 1")
+# # print("Python Programming")
+# # print("PYTHON 4 EVERYONE")
+# # print("Python is easy to learn")
+# # print("Python is easy to learn")
+# # print(1+1)
+# # print("Hello rayyan and nabeedah")
 
-# we are now doing file handling in python 
+# # we are now doing file handling in python 
 
+
+# # writing and reading in python
 # with open('D:/task.txt', 'r') as file:
 #     content = file.read()
 #     print(content)
@@ -18,14 +20,37 @@
 #     file.write("\n")
 #     file.write("Python is easy to learn")
 
+
+# # counting the number of lines in the file
 # f = open('D:/task.txt', 'r')
 # f = f.readlines()
 # print(len(f))
 
-f = open('D:/task.txt', 'r')
-f = f.read()
-change = f.replace('()', '[]')
 
-with open('D:/task.txt', 'w') as file:
-    file.write(change)
-    print(change)
+
+
+
+# # replacing the brackets in the file
+# f = open('D:/task.txt', 'r')
+# f = f.read()
+# change = f.replace(')', ']')
+# change = change.replace('(', '[')
+
+# with open('D:/task.txt', 'w') as file:
+#     file.write(change)
+#     print(change)
+
+
+# using w+ mode to write and read the file
+# with open('D:/test 2.txt', 'w+') as file:
+#     file.write("this is the file number 2" )
+#     file.write("\n")
+#     file.write("Python vs c++")
+#     content = file.read()
+#     print(content)
+
+# using r+ mode to read and write the file
+with open('D:/test 2.txt', 'r+') as file:
+    file.append("Python vs c++")
+    content = file.read()
+    print(content)
